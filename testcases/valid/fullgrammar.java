0e82010a250d855b3c4bb32c817276f48af3ca01
Victor Koronen <koronen@kth.se>
2014-02-25 17:23:09 +0100

    Add beginnings of a test harness

---
+++ testcases/valid/fullgrammar.java
@@ -0,0 +1,28 @@
+class Main {
+    public static void main(String[] args) {
+        Aux aux;
+        int[] a;
+        aux = new Aux();
+        a = new int[10];
+        aux.doTheThing(a);
+        System.out.println(0);
+    }
+}
+
+class Aux {
+    boolean foo;
+
+    public Aux doTheThing(int[] arr)
+    {
+        int i;
+        i = 0;
+        while(i < 0)
+        {
+            if(!true)
+            {
+                arr[i] = 1;
+            }
+        }
+        return this;
+    }
+}
