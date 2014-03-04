8e4d89e288b7d0ba198bf1e65d6b1ceda7e238d9
Victor Koronen <koronen@kth.se>
2014-03-04 13:15:20 +0100

    Skip comments
    Source:
    <https://stackoverflow.com/questions/7904755/another-way-to-correctly-skip-single-line-comments-in-javacc-n>

---
+++ testcases/valid/comments.java
@@ -0,0 +1,15 @@
+/**
+ * JavaDoc goes here.
+ *
+ * @author Martin Barksten & Victor Koronen
+ */
+class Foo {
+    /**
+     * More JavaDoc.
+     */
+    public static void main(String[] args) {
+        // A comment on a separate line
+        int a; // A comment after a vardecl
+        a = (a) + 3;
+    }
+}
