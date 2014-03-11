89a61c2da9aca9df5df883ee7c249981ef45faa0
Victor Koronen <koronen@kth.se>
2014-03-11 12:18:53 +0100

    Implement more of DepthFirstVisitor

--- src/visitor/DepthFirstVisitor.java
+++ src/visitor/DepthFirstVisitor.java
@@ -12,8 +12,10 @@ import syntaxtree.ClassDecl;
 import syntaxtree.ClassDeclExtends;
 import syntaxtree.ClassDeclSimple;
 import syntaxtree.Exp;
+import syntaxtree.ExpList;
 import syntaxtree.False;
 import syntaxtree.Formal;
+import syntaxtree.FormalList;
 import syntaxtree.Identifier;
 import syntaxtree.IdentifierExp;
 import syntaxtree.IdentifierType;
@@ -24,6 +26,7 @@ import syntaxtree.IntegerType;
 import syntaxtree.LessThan;
 import syntaxtree.MainClass;
 import syntaxtree.MethodDecl;
+import syntaxtree.MethodDeclList;
 import syntaxtree.Minus;
 import syntaxtree.NewArray;
 import syntaxtree.NewObject;
@@ -32,10 +35,13 @@ import syntaxtree.Plus;
 import syntaxtree.Print;
 import syntaxtree.Program;
 import syntaxtree.Statement;
+import syntaxtree.StatementList;
 import syntaxtree.This;
 import syntaxtree.Times;
 import syntaxtree.True;
+import syntaxtree.Type;
 import syntaxtree.VarDecl;
+import syntaxtree.VarDeclList;
 import syntaxtree.While;
 
 public class DepthFirstVisitor implements Visitor {
@@ -45,11 +51,7 @@ public class DepthFirstVisitor implements Visitor {
         visit(n.m);
         for (int i = 0; i < n.cl.size(); i++) {
             ClassDecl cd = n.cl.elementAt(i);
-            if (cd instanceof ClassDeclSimple) {
-                visit((ClassDeclSimple) cd);
-            } else if (cd instanceof ClassDeclExtends) {
-                visit((ClassDeclExtends) cd);
-            }
+            visit(cd);
         }
     }
 
@@ -62,74 +64,75 @@ public class DepthFirstVisitor implements Visitor {
 
     @Override
     public void visit(ClassDeclSimple n) {
-        // TODO Auto-generated method stub
-
+        visit(n.i);
+        visit(n.vl);
+        visit(n.ml);
     }
 
     @Override
     public void visit(ClassDeclExtends n) {
-        // TODO Auto-generated method stub
-
+        throw new UnsupportedOperationException(n.getClass().getName()
+                + " not implemented yet!");
     }
 
     @Override
     public void visit(VarDecl n) {
-        // TODO Auto-generated method stub
-
+        visit(n.t);
+        visit(n.i);
     }
 
     @Override
     public void visit(MethodDecl n) {
-        // TODO Auto-generated method stub
-
+        visit(n.t);
+        visit(n.i);
+        visit(n.fl);
+        visit(n.vl);
+        visit(n.sl);
+        visit(n.e);
     }
 
     @Override
     public void visit(Formal n) {
-        // TODO Auto-generated method stub
-
+        visit(n.t);
+        visit(n.i);
     }
 
     @Override
     public void visit(IntArrayType n) {
-        // TODO Auto-generated method stub
-
+        // No-op
     }
 
     @Override
     public void visit(BooleanType n) {
-        // TODO Auto-generated method stub
-
+        // No-op
     }
 
     @Override
     public void visit(IntegerType n) {
-        // TODO Auto-generated method stub
-
+        // No-op
     }
 
     @Override
     public void visit(IdentifierType n) {
-        // TODO Auto-generated method stub
-
+        // No-op
     }
 
     @Override
     public void visit(Block n) {
-        // TODO Auto-generated method stub
-
+        visit(n.sl);
     }
 
     @Override
     public void visit(If n) {
-        // TODO Auto-generated method stub
-
+        visit(n.e);
+        visit(n.s1);
+        visit(n.s2);
     }
 
     @Override
     public void visit(While n) {
-        // TODO Auto-generated method stub
-
+        visit(n.e);
+        visit(n.s);
     }
 
     @Override
@@ -139,127 +142,224 @@ public class DepthFirstVisitor implements Visitor {
 
     @Override
     public void visit(Assign n) {
-        // TODO Auto-generated method stub
-
+        visit(n.i);
+        visit(n.e);
     }
 
     @Override
     public void visit(ArrayAssign n) {
-        // TODO Auto-generated method stub
-
+        visit(n.i);
+        visit(n.e1);
+        visit(n.e2);
     }
 
     @Override
     public void visit(And n) {
-        // TODO Auto-generated method stub
-
+        visit(n.e1);
+        visit(n.e2);
     }
 
     @Override
     public void visit(LessThan n) {
-        // TODO Auto-generated method stub
-
+        visit(n.e1);
+        visit(n.e2);
     }
 
     @Override
     public void visit(Plus n) {
-        // TODO Auto-generated method stub
-
+        visit(n.e1);
+        visit(n.e2);
     }
 
     @Override
     public void visit(Minus n) {
-        // TODO Auto-generated method stub
-
+        visit(n.e1);
+        visit(n.e2);
     }
 
     @Override
     public void visit(Times n) {
-        // TODO Auto-generated method stub
-
+        visit(n.e1);
+        visit(n.e2);
     }
 
     @Override
     public void visit(ArrayLookup n) {
-        // TODO Auto-generated method stub
-
+        visit(n.e1);
+        visit(n.e2);
     }
 
     @Override
     public void visit(ArrayLength n) {
-        // TODO Auto-generated method stub
-
+        visit(n.e);
     }
 
     @Override
     public void visit(Call n) {
-        // TODO Auto-generated method stub
-
+        visit(n.e);
+        visit(n.i);
+        visit(n.el);
     }
 
     @Override
     public void visit(IntegerLiteral n) {
-        // TODO Auto-generated method stub
-
+        // No-op
     }
 
     @Override
     public void visit(True n) {
-        // TODO Auto-generated method stub
-
+        // No-op
     }
 
     @Override
     public void visit(False n) {
-        // TODO Auto-generated method stub
-
+        // No-op
     }
 
     @Override
     public void visit(IdentifierExp n) {
-        // TODO Auto-generated method stub
-
+        // No-op
     }
 
     @Override
     public void visit(This n) {
-        // TODO Auto-generated method stub
-
+        // No-op
     }
 
     @Override
     public void visit(NewArray n) {
-        // TODO Auto-generated method stub
-
+        visit(n.e);
     }
 
     @Override
     public void visit(NewObject n) {
-        // TODO Auto-generated method stub
-
+        visit(n.i);
     }
 
     @Override
     public void visit(Not n) {
-        // TODO Auto-generated method stub
-
+        visit(n.e);
     }
 
     @Override
     public void visit(Identifier n) {
-        // TODO Auto-generated method stub
-
+        // No-op
     }
 
     private void visit(Exp e) {
         if (e instanceof IntegerLiteral) {
             visit((IntegerLiteral) e);
+        } else if (e instanceof Plus) {
+            visit((Plus) e);
+        } else if (e instanceof ArrayLookup) {
+            visit((ArrayLookup) e);
+        } else if (e instanceof Call) {
+            visit((Call) e);
+        } else if (e instanceof IdentifierExp) {
+            visit((IdentifierExp) e);
+        } else if (e instanceof NewObject) {
+            visit((NewObject) e);
+        } else if (e instanceof NewArray) {
+            visit((NewArray) e);
+        } else if (e instanceof LessThan) {
+            visit((LessThan) e);
+        } else if (e instanceof Not) {
+            visit((Not) e);
+        } else if (e instanceof True) {
+            visit((True) e);
+        } else if (e instanceof False) {
+            visit((False) e);
+        } else if (e instanceof And) {
+            visit((And) e);
+        } else if (e instanceof ArrayLength) {
+            visit((ArrayLength) e);
+        } else if (e instanceof Times) {
+            visit((Times) e);
+        } else if (e instanceof This) {
+            visit((This) e);
+        } else {
+            throw new UnsupportedOperationException("visit(Exp: "
+                    + e.getClass().getName() + ") not implemented yet!");
         }
     }
 
     private void visit(Statement s) {
         if (s instanceof Print) {
             visit((Print) s);
+        } else if (s instanceof Block) {
+            visit((Block) s);
+        } else if (s instanceof Assign) {
+            visit((Assign) s);
+        } else if (s instanceof While) {
+            visit((While) s);
+        } else if (s instanceof If) {
+            visit((If) s);
+        } else if (s instanceof ArrayAssign) {
+            visit((ArrayAssign) s);
+        } else {
+            throw new UnsupportedOperationException("visit(Statement: "
+                    + s.getClass().getName() + ") not implemented yet!");
+        }
+    }
+
+    private void visit(ClassDecl cd) {
+        if (cd instanceof ClassDeclSimple) {
+            visit((ClassDeclSimple) cd);
+        } else if (cd instanceof ClassDeclExtends) {
+            visit((ClassDeclExtends) cd);
+        } else {
+            throw new UnsupportedOperationException("visit(ClassDecl: "
+                    + cd.getClass().getName() + ") not implemented yet!");
+        }
+    }
+
+    private void visit(MethodDeclList ml) {
+        for (int i = 0; i < ml.size(); i++) {
+            MethodDecl md = ml.elementAt(i);
+            visit(md);
+        }
+    }
+
+    private void visit(VarDeclList vl) {
+        for (int i = 0; i < vl.size(); i++) {
+            VarDecl vd = vl.elementAt(i);
+            visit(vd);
+        }
+    }
+
+    private void visit(ExpList el) {
+        for (int i = 0; i < el.size(); i++) {
+            Exp e = el.elementAt(i);
+            visit(e);
+        }
+    }
+
+    private void visit(Type t) {
+        if (t instanceof IntArrayType) {
+            visit((IntArrayType) t);
+        } else if (t instanceof BooleanType) {
+            visit((BooleanType) t);
+        } else if (t instanceof IntegerType) {
+            visit((IntegerType) t);
+        } else if (t instanceof IdentifierType) {
+            visit((IdentifierType) t);
+        } else {
+            throw new UnsupportedOperationException("visit(Type: "
+                    + t.getClass().getName() + ") not implemented yet!");
+        }
+    }
+
+    private void visit(StatementList sl) {
+        for (int i = 0; i < sl.size(); i++) {
+            Statement s = sl.elementAt(i);
+            visit(s);
+        }
+    }
+
+    private void visit(FormalList fl) {
+        for (int i = 0; i < fl.size(); i++) {
+            Formal f = fl.elementAt(i);
+            visit(f);
         }
     }
 
