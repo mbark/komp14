2fbbf94d1e8c568a008470bbcf9a9c7d3e61999e
Victor Koronen <koronen@kth.se>
2014-03-18 14:54:21 +0100

    Make Parser class public
    To allow for instantiating it outside the mjc package.

--- src/mjc/JVMMain.java
+++ src/mjc/JVMMain.java
@@ -2,18 +2,19 @@ package mjc;
 
 import java.io.StringReader;
 
+import symbol.TypeCheckPhaseOneVisitor;
 import syntaxtree.Program;
-import syntaxtree.SyntaxTreePrinter;
 
 public class JVMMain {
 
     public static void main(String[] args) {
         try {
             Program p = new Parser(new StringReader(args[0])).Program();
-            SyntaxTreePrinter stp = new SyntaxTreePrinter(System.out);
+            TypeCheckPhaseOneVisitor stp = new TypeCheckPhaseOneVisitor();
             stp.visit(p);
         } catch (Throwable e) {
             System.out.println("Syntax check failed: " + e.getMessage());
+            e.printStackTrace(System.out);
             System.exit(1);
         }
     }
