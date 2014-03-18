2efbe1d89dfbd6de3fa1c6c65129da842c0073d7
mbark <barksten@kth.se>
2014-03-18 10:53:10 +0100

    Add Symbol and Table interfaces

--- src/symbol/Symbol.java
+++ src/symbol/Symbol.java
@@ -1,5 +1,11 @@
 package symbol;
 
 public class Symbol {
+    public String toString() {
+        return "";
+    }
 
+    public static Symbol symbol(String s) {
+        return null;
+    }
 }
---
+++ src/symbol/Table.java
@@ -0,0 +1,29 @@
+package symbol;
+
+import java.util.Enumeration;
+
+public class Table {
+
+    public Table() {
+    }
+
+    public void put(Symbol key, Object value) {
+
+    }
+
+    public Object get(Symbol key) {
+        return null;
+    }
+
+    public void beginScope() {
+
+    }
+
+    public void endScope() {
+
+    }
+
+    public Enumeration keys() {
+        return null;
+    }
+}
