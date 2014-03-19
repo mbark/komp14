2bdd25d6cc08d7d9239e383caa79d284cd2cf09b
mbark <barksten@kth.se>
2014-03-19 11:23:47 +0100

    Consider the order of method params

--- src/symbol/MethodTable.java
+++ src/symbol/MethodTable.java
@@ -1,7 +1,9 @@
 package symbol;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 
 import syntaxtree.Type;
 
@@ -9,14 +11,14 @@ public class MethodTable {
     private Symbol id;
     private Type returnType;
 
-    private HashMap<Symbol, Type> params;
+    private List<Param> params;
     private HashMap<Symbol, Type> locals;
 
     public MethodTable(Symbol id, Type returnType) {
         this.id = id;
         this.returnType = returnType;
 
-        params = new HashMap<Symbol, Type>();
+        params = new ArrayList<>();
         locals = new HashMap<Symbol, Type>();
     }
 
@@ -29,19 +31,44 @@ public class MethodTable {
     }
 
     public boolean putParam(Symbol key, Type value) {
-        if (params.containsKey(key)) {
+        Param param = new Param(key, value);
+        if (containsParam(key)) {
             return false;
         }
-        params.put(key, value);
+        
+        params.add(param);
         return true;
+    }
+    
+    private static final class Param {
+        Symbol key;
+        Type value;
+        private Param(Symbol key, Type value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 
     public Type getParam(Symbol key) {
-        return params.get(key);
+        for(Param param : params) {
+            if(param.key.equals(key)) {
+                return param.value;
+            }
+        }
+        return null;
+    }
+    
+    private boolean containsParam(Symbol key) {
+        for(Param param : params) {
+            if(param.key.equals(key)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public boolean putLocal(Symbol key, Type value) {
-        if (params.containsKey(key) || locals.containsKey(key)) {
+        if (containsParam(key) || locals.containsKey(key)) {
             return false;
         }
         locals.put(key, value);
