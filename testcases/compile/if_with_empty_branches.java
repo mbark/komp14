fd4149556ac1b11908ed8da6cb3f4818146de34a
Victor Koronen <koronen@kth.se>
2014-03-21 13:42:37 +0100

    Add two test cases for if

---
+++ testcases/compile/if_with_empty_branches.java
@@ -0,0 +1,5 @@
+class IfWithEmptyBranches {
+    public static void main(String[] args) {
+        if(true) { } else { }
+    }
+}
---
+++ testcases/noncompile/if_without_else.java
@@ -0,0 +1,9 @@
+// EXT:!IWE
+class IfWithoutElse {
+    public static void main(String[] args) {
+        if(false)
+        {
+            System.out.println("No!");
+        }
+    }
+}
