7bfb63df8b84c8b3c1125d1899729f0ea479d52f
Victor Koronen <koronen@kth.se>
2014-04-08 15:23:09 +0200

    Remove duplicate equals method in tree.Type

--- src/syntaxtree/Type.java
+++ src/syntaxtree/Type.java
@@ -6,9 +6,6 @@ import visitor.TypeVisitor;
 import visitor.Visitor;
 
 public abstract class Type {
-    public boolean equals(Type tp) {
-        return getClass().equals(tp.getClass());
-    }
 
     public abstract void accept(Visitor v);
 
